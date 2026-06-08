e4eb9efb854ab83bda166bf6b6c38a2dd952822d
Nick <nick.esselman@gmail.com>
2026-06-08 12:48:13 +0200

    AA

--- led.py
+++ led.py
@@ -1,36 +1,26 @@
-from gpiozero import Button, LED
+from gpiozero import LED
+from signal import pause
 
-
-# GPIO pins
-SENSOR_LED_PIN = 18
-TOGGLE_LED_BUTTON_PIN = 23
-EXIT_BUTTON_PIN = 24
 
 # Extra LEDs
 EXTRA_LED_PINS = [25, 8, 7, 1]
 
-
-sensor_led = LED(SENSOR_LED_PIN)
 extra_leds = [LED(pin) for pin in EXTRA_LED_PINS]
 
-toggle_led_button = Button(
-    TOGGLE_LED_BUTTON_PIN,
-    pull_up=True,
-    bounce_time=0.2
-)
 
-exit_button = Button(
-    EXIT_BUTTON_PIN,
-    pull_up=True,
-    bounce_time=0.2
-)
+print("Turning LEDs on:")
+print("GPIO 25, GPIO 8, GPIO 7, GPIO 1")
+print("Press Ctrl+C to stop.")
 
-
-sensor_led_on = True
-running = True
-
-sensor_led.on()
-
-# Turn all extra LEDs on
 for led in extra_leds:
     led.on()
+
+try:
+    pause()
+
+except KeyboardInterrupt:
+    print("\nStopping.")
+
+finally:
+    for led in extra_leds:
+        led.off()
